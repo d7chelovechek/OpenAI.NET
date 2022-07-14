deae4a3b2f938995e5edfac933d8856dc2caf938
Ayrat Khaydarov <info.chelovechek@gmail.com>
2022-07-14 16:48:20 +0300

    Issue #14
    ● Implemented ability to send requests to API in different languages ​​supported by Google Translate.

---
+++ OpenAI.NET.Web/Translators/GoogleTranslator.cs
@@ -0,0 +1,73 @@
+﻿using OpenAI.NET.Web.Translators.Interfaces;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace OpenAI.NET.Web.Translators
+{
+    public class GoogleTranslator : ITranslator
+    {
+        private const string _mainLanguage = "en";
+
+        private readonly HttpClient _client;
+
+        public GoogleTranslator()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<string> TranslateFromEnglish(string text, string language)
+        {
+            try
+            {
+                if (language is not _mainLanguage)
+                {
+                    string response = await _client.GetStringAsync(
+                        $"https://translate.googleapis.com/translate_a/single?" +
+                        $"client=gtx&sl={_mainLanguage}&tl={language}&dt=t&q={Uri.EscapeDataString(text)}");
+
+                    response = response.Replace($"\"{_mainLanguage}\",", "");
+
+                    return JsonConvert.DeserializeObject<List<List<List<dynamic>>>>(
+                        response)[0][0][0].ToString();
+                }
+                else
+                {
+                    return text;
+                }
+            }
+            catch
+            {
+                throw new Exception("Failed to translate text from English");
+            }
+        }
+
+        public async Task<string> TranslateIntoEnglish(string text, string language)
+        {
+            try
+            {
+                if (language is not _mainLanguage)
+                {
+                    string response = await _client.GetStringAsync(
+                        $"https://translate.googleapis.com/translate_a/single?" +
+                        $"client=gtx&sl={language}&tl={_mainLanguage}&dt=t&q={Uri.EscapeDataString(text)}");
+
+                    response = response.Replace($"\"{language}\",", "");
+
+                    return JsonConvert.DeserializeObject<List<List<List<dynamic>>>>(
+                        response)[0][0][0].ToString();
+                }
+                else
+                {
+                    return text;
+                }
+            }
+            catch
+            {
+                throw new Exception("Failed to translate text into English");
+            }
+        }
+    }
+}
---
+++ OpenAI.NET.Web/Translators/Interfaces/ITranslator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace OpenAI.NET.Web.Translators.Interfaces
+{
+    public interface ITranslator
+    {
+        public Task<string> TranslateIntoEnglish(string text, string language);
+        public Task<string> TranslateFromEnglish(string text, string language);
+    }
+}
